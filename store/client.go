c310d4b7f7b347849320afb38cb83b408f683b4b
Yaniv Schwartz <syaniv@live.com>
2020-06-16 14:31:38 -0700

    implement data access

--- store/client.go
+++ store/client.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/gofrs/uuid"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/nivista/tasktimer/timer"
 )
@@ -15,8 +15,44 @@ type Client struct {
 	*pgxpool.Pool
 }
 
-func NewClient() (*Client, error) {
-	dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+type (
+	taskType     string
+	scheduleType string
+)
+
+const (
+	http taskType = "HTTP"
+)
+
+const (
+	cron     scheduleType = "CRON"
+	interval scheduleType = "INTERVAL"
+)
+
+func (t *taskType) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("Expected []byte in Scan of taskType")
+	}
+
+	*t = taskType(string(bytes)) // could i go straight to tasktype?
+
+	return nil
+}
+
+func (t *scheduleType) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("Expected []byte in Scan of scheduleType")
+	}
+
+	*t = scheduleType(string(bytes))
+
+	return nil
+}
+
+func NewClient(ctx context.Context) (*Client, error) {
+	dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_CONN"))
 	if err != nil {
 		return nil, err
 	}
@@ -25,29 +61,37 @@ func NewClient() (*Client, error) {
 }
 
 func (c *Client) CreateTimer(ctx context.Context, t timer.Timer) error {
-	var taskType string
+	var tt taskType
 	switch task := t.Task.(type) {
-	case timer.HTTP:
-		taskType = "http"
+	case *timer.HTTP:
+		tt = http
 	default:
 		panic(fmt.Sprintf("CreateTimer doesn't know about %T", task))
 	}
 
-	var scheduleType string
+	var st scheduleType
 	switch schedule := t.Schedule.(type) {
-	case timer.Cron:
-		scheduleType = "cron"
-	case timer.Interval:
-		scheduleType = "interval"
+	case *timer.Cron:
+		st = cron
+	case *timer.Interval:
+		st = interval
 	default:
 		panic(fmt.Sprintf("CreateTimer doesn't know about %T", schedule))
 	}
 
-	_, err := c.Query(ctx,
+	task, err := t.Task.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	sched, err := t.Schedule.MarshalBinary()
+	if err != nil {
+		return err
+	}
+
+	_, err = c.Query(ctx,
 		`insert into timers (id, account, executionCount, taskType, task, scheduleType, schedule, meta) 
-			values ($1, $2, $3, $4, $5, $6)
-			where exists select id from timers where account=$2 and id=$1`,
-		t.ID, t.Account, t.ExecutionCount, taskType, t.Task, scheduleType, t.Schedule, t.Meta)
+			values ($1, $2, $3, $4, $5, $6, $7, $8)`,
+		t.ID, t.Account, t.ExecutionCount, tt, task, st, sched, t.Meta)
 
 	return err
 }
@@ -62,42 +106,46 @@ func (c *Client) DeleteTimer(ctx context.Context, account string, id uuid.UUID) 
 func (c *Client) GetTimer(ctx context.Context, account string, id uuid.UUID) (*timer.Timer, error) {
 	t := timer.Timer{}
 
-	var taskType, scheduleType string
-	var task, schedule interface{}
+	var tt taskType
+	var st scheduleType
+	var taskBytes, scheduleBytes []byte
 
 	err := c.QueryRow(ctx,
-		`select id, account, executionCount, taskType, task, scheduleType, schedule, meta where account=$1 and id=$2`,
-		account, id).Scan(&t.ID, &t.Account, &t.ExecutionCount, &taskType, &task, &scheduleType, &schedule)
+		`select id, account, executionCount, taskType, task, scheduleType, schedule, meta from timers 
+			where account=$1 and id=$2`,
+		account, id).Scan(&t.ID, &t.Account, &t.ExecutionCount, &tt, &taskBytes, &st, &scheduleBytes, &t.Meta)
 
 	if err != nil {
 		return nil, err
 	}
 
-	switch taskType {
-	case "http":
-
-		http, ok := task.(timer.HTTP)
-		if !ok {
-			return nil, errors.New("Failed type declaration") // TODO, make custom error types
+	switch tt {
+	case http:
+		var task timer.HTTP
+		err = task.UnmarshalBinary(taskBytes)
+		if err != nil {
+			return nil, err // TODO, make custom error types
 		}
-		t.Task = http
+		t.Task = &task
 	default:
 		return nil, errors.New("Unknown type")
 	}
 
-	switch scheduleType {
-	case "cron":
-		cron, ok := task.(timer.Cron)
-		if !ok {
-			return nil, errors.New("Failed type declaration")
+	switch st {
+	case cron:
+		var schedule timer.Cron
+		err = schedule.UnmarshalBinary(taskBytes)
+		if err != nil {
+			return nil, err
 		}
-		t.Schedule = cron
-	case "interval":
-		interval, ok := task.(timer.Interval)
-		if !ok {
-			return nil, errors.New("Failed type declaration")
+		t.Schedule = &schedule
+	case interval:
+		var schedule timer.Interval
+		err := schedule.UnmarshalBinary(scheduleBytes)
+		if err != nil {
+			return nil, err
 		}
-		t.Schedule = interval
+		t.Schedule = &schedule
 	default:
 		return nil, errors.New("Unknown type")
 	}
